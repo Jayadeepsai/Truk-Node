00ac8abb1cac10d1066aa6b53c3a52f2bebc5116
Jayadeepsai <jayadeepsaim@gmail.com>
2023-02-18 12:23:06 +0530

    18-02-2023

--- models/generateQuote.js
+++ models/generateQuote.js
@@ -1,13 +1,27 @@
 //get the reuired libraries
-const mongoose= require("mongoose");
+const mongoose = require("mongoose");
 
 //introduce bigs array as part of this quote generate schema for accesing the bids later
-const bidsData=mongoose.Schema({
+const bidsData = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    mobileNo:{type: Number},
-    Bidprice: {type: Array},
-    Negotiate:{type: Array},
-    tentativefinalPrice:{type: Number}
+    mobileNo: { type: Number },
+    BidActivity: { type: Array },
+    Bidprice: { type: Array },
+    Negotiate: { type: Array },
+    tentativefinalPrice: { type: Number },
+    /*initialAccept:{
+        type:String,
+        default:"Not accepted"
+            },*/
+
+    isAgentAccepted:{
+        type:Boolean,
+        default:false
+    } ,
+    isShipperAccepted:{
+type:Boolean,
+default:false
+    }       
 })
 
 
@@ -15,80 +29,89 @@ const bidsData=mongoose.Schema({
 //define the schema here
 const generateQuoteSchema = mongoose.Schema({
     //define the object ID
-   _id: mongoose.Schema.Types.ObjectId,
-   OriginLocation:{
-    type:String,
-    //required:true
-},
-DestinationLocation:{
-    type:String,
-   // required:true
-},
-Number:{
-    type:String,
-   // required:true
-},
-product:{
-    type:Array,
-    //required:true
-},
-Quantity:{
-    type:String,
-    //required:true
-},
+    _id: mongoose.Schema.Types.ObjectId,
+    OriginLocation: {
+        type: String,
+        //required:true
+    },
+    DestinationLocation: {
+        type: String,
+        // required:true
+    },
+    state: {
 
-// vehicle:{
-//     type:Array,
-//     required:true
-// },
+        type: Array
+    },
+    Number: {
+        type: String,
+        // required:true
+    },
+    product: {
+        type: Array,
+        //required:true
+    },
+    Quantity: {
+        type: String,
+        //required:true
+    },
 
-expectedPrice:{
-    type:String,
-   // required:true
-},
+    // vehicle:{
+    //     type:Array,
+    //     required:true
+    // },
 
-date:{
-    type:String,
-    //required:true
-},
- 
-typeOfPay:{
-    type:Array,
-    //required:true
-},
+    expectedPrice: {
+        type: String,
+        // required:true
+    },
 
-length:{
-    type:String,
-   // required:true
-},
+    date: {
+        type: String,
+        //required:true
+    },
 
-breadth:{
-    type:String,
-   // required:true
-},
+    typeOfPay: {
+        type: Array,
+        //required:true
+    },
 
-height:{
-    type:String,
-   // required:true
-},
+    length: {
+        type: String,
+        // required:true
+    },
 
-comments:{
-    type:String,
-   // required:true
-},
+    breadth: {
+        type: String,
+        // required:true
+    },
 
-data:{
-    type:String
-},
+    height: {
+        type: String,
+        // required:true
+    },
 
-isActive:{
-    type:String,
-    default:'Active'
-},
-   
-   
-   quoteSentTo:{type: Array, required: true},
-   bids:[bidsData]
+    comments: {
+        type: String,
+        // required:true
+    },
+
+    data: {
+        type: String
+    },
+
+    isActive: {
+        type: String,
+        default: 'Active'
+    },
+    userAcceptedPrice: {
+        type: String,
+        default:"0"
+    },
+    
+
+
+    quoteSentTo: { type: Array, required: true },
+    bids: [bidsData]
 });
 
 
